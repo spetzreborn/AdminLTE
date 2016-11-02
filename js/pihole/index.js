b9ae9c477231ffe7e93abe1c0989a8d0a2ae6a9d
Mcat12 <newtoncat12@yahoo.com>
2016-11-02 19:03:49 -0400

    Queries over time is getting there
    It shows up now at least.

--- js/pihole/index.js
+++ js/pihole/index.js
@@ -1,7 +1,46 @@
 $(document).ready(function() {
-    // Pull in data via AJAX
-
     updateSummaryData();
+
+    // Create charts
+    var chartData = {
+        labels: [],
+        datasets: [
+            {
+                label: "All Queries",
+                data: [],
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(0, 166, 90,.8)",
+                pointColor: "rgba(0, 166, 90,.8)"
+            },
+            {
+                label: "Ad Queries",
+                data: [],
+                fillColor: "rgba(0,192,239,0.5)",
+                strokeColor: "rgba(0,192,239,1)",
+                pointColor: "rgba(0,192,239,1)"
+            }
+        ]
+    };
+
+    var animate = false;
+    timeLineChart = new Chart($("#queryOverTimeChart"), {
+        type: "line",
+        data: chartData,
+        pointDot : false,
+        animation : animate
+    });
+
+    queryTypeChart = new Chart($("#queryTypeChart"), {
+        type: "doughnut",
+        data: chartData,
+        animation : animate
+    });
+
+    forwardDestinationChart = new Chart($("#forwardDestinationChart"), {
+        type: "doughnut",
+        data: chartData,
+        animation : animate
+    });
 
     updateQueriesOverTime();
 
@@ -12,74 +51,10 @@ $(document).ready(function() {
     updateForwardDestinations();
 
     updateTopLists();
-
-    // Create charts
-    var chartData = {
-        labels: [],
-        datasets: [
-            {
-                label: "All Queries",
-                fillColor: "rgba(220,220,220,0.5)",
-                strokeColor: "rgba(0, 166, 90,.8)",
-                pointColor: "rgba(0, 166, 90,.8)"
-            },
-            {
-                label: "Ad Queries",
-                fillColor: "rgba(0,192,239,0.5)",
-                strokeColor: "rgba(0,192,239,1)",
-                pointColor: "rgba(0,192,239,1)"
-            }
-        ]
-    };
-    var isMobile = {
-        Windows: function() {
-            return /IEMobile/i.test(navigator.userAgent);
-        },
-        Android: function() {
-            return /Android/i.test(navigator.userAgent);
-        },
-        BlackBerry: function() {
-            return /BlackBerry/i.test(navigator.userAgent);
-        },
-        iOS: function() {
-            return /iPhone|iPad|iPod/i.test(navigator.userAgent);
-        },
-        any: function() {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
-        }
-    };
-    var animate = false;
-    var ctx = document.getElementById("queryOverTimeChart").getContext("2d");
-    timeLineChart = new Chart(ctx).Line(chartData,
-        {
-            pointDot : false,
-            legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
-            animation : animate
-        }
-    );
-
-    ctx = document.getElementById("queryTypeChart").getContext("2d");
-    queryTypeChart = new Chart(ctx).Doughnut([],
-        {
-            legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].strokeColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
-            animation : animate
-        }
-    );
-
-    ctx = document.getElementById("forwardDestinationChart").getContext("2d");
-    forwardDestinationChart = new Chart(ctx).Doughnut([],
-        {
-            legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].strokeColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
-            animation : animate
-        }
-    );
 });
-
-// Functions to oupdate data in page
 
 function updateSummaryData(runOnce) {
     $.getJSON("api.php?summary", function LoadSummaryData(data) {
-        //$("h3.statistic").addClass("glow");
         if ($("h3#ads_blocked_today").text() != data.ads_blocked_today) {
             $("h3#ads_blocked_today").addClass("glow");
         }
@@ -95,24 +70,23 @@ function updateSummaryData(runOnce) {
             $("h3#dns_queries_today").text(data.dns_queries_today);
             $("h3#domains_being_blocked").text(data.domains_being_blocked);
             $("h3#ads_percentage_today").text(data.ads_percentage_today + "%");
-            $("h3.statistic.glow").removeClass("glow")
+            $("h3.statistic.glow").removeClass("glow");
         }, 500);
     }).done(function() {
-        if (runOnce !== true) {
-            setTimeout(updateSummaryData, 10000);
-        }
+        setTimeout(updateSummaryData, 10000);
     }).fail(function() {
-        if (runOnce !== true) {
-            setTimeout(updateSummaryData, (1000 * 60 * 5));
-        }
+        setTimeout(updateSummaryData, (1000 * 60 * 5));
     });;
 }
 
 function updateQueriesOverTime() {
     $.getJSON("api.php?overTimeData", function(data) {
         for (hour in data.ads_over_time) {
-            timeLineChart.addData([data.domains_over_time[hour], data.ads_over_time[hour]], hour + ":00");
+            timeLineChart.data.datasets[0].data.push(data.domains_over_time[hour]);
+            timeLineChart.data.datasets[1].data.push(data.ads_over_time[hour]);
+            timeLineChart.data.labels.push(hour + ":00");
         }
+        timeLineChart.update();
         $('#queries-over-time .overlay').remove();
         //$('#queries-over-time').append(timeLineChart.generateLegend());
     });
